bba0d79053083a80be9a9fb81bdfa4e44e04f5b0
Fred <bucrepinfo@gmail.com>
2026-07-01 18:25:54 +0300

    fix(mocks): restore mock package barrel exports

--- packages/mocks/src/index.ts
+++ packages/mocks/src/index.ts
@@ -3,6 +3,6 @@ export * from './customers';
 export * from './policies';
 export * from './claims';
 export * from './finance';
-export * from "./workflows";
+export * from './workflows';
 export * from './platform';
 export * from './notifications';
