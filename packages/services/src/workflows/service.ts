23bafe6559b22ad59b0d67cfbd0f26ef3732b752
Fred <bucrepinfo@gmail.com>
2026-07-01 12:18:47 +0300

    feat(services): add workflow task service methods

--- packages/services/src/workflows/service.ts
+++ packages/services/src/workflows/service.ts
@@ -1,6 +1,22 @@
-import { mockWorkflows } from "@insuros/mocks";
+import { mockWorkflowTasks, mockWorkflows } from "@insuros/mocks";
 
 export class WorkflowService {
+async getTasks() {
+  return mockWorkflowTasks;
+}
+
+async getTask(id: string) {
+  return mockWorkflowTasks.find((task) => task.id === id);
+}
+
+async getTasksByWorkflow(workflowId: string) {
+  return mockWorkflowTasks.filter((task) => task.workflowId === workflowId);
+}
+
+async getBlockedTasks() {
+  return mockWorkflowTasks.filter((task) => task.status === "Blocked");
+}
+
   async getWorkflows() {
     return mockWorkflows;
   }
