797cedfda3a3e05ec9a7c20f843cef98fd8386a9
Fred <bucrepinfo@gmail.com>
2026-07-02 21:51:16 +0300

    refactor(domain): use shared workflow type for transition rules

--- packages/domain/src/workflow-rule.ts
+++ packages/domain/src/workflow-rule.ts
@@ -1,8 +1,9 @@
-import type { WorkflowTransitionAction } from './workflow-transition';
+import type { WorkflowTransitionAction } from "./workflow-transition";
+import type { WorkflowType } from "./workflow-type";
 
 export interface WorkflowTransitionRule {
   id: string;
-  workflowType: string;
+  workflowType: WorkflowType;
   fromStatus: string;
   action: WorkflowTransitionAction;
   toStatus: string;
