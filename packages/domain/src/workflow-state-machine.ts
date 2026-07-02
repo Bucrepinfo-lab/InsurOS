8df8d5ab262f5e89c5499b6b63f1bc24ecacff3f
Fred <bucrepinfo@gmail.com>
2026-07-02 20:06:56 +0300

    feat(domain): add workflow state machine

--- packages/domain/src/workflow-state-machine.ts
+++
@@ -1,7 +0,0 @@
-import type { WorkflowTransitionAction } from './workflow-transition';
-
-export interface WorkflowStateMachine {
-  workflowType: string;
-  currentStatus: string;
-  allowedActions: WorkflowTransitionAction[];
-}
