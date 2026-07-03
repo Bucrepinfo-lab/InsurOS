053157ed9412cfce2456e1ae21d2f2c24cf82381
Fred <bucrepinfo@gmail.com>
2026-07-03 22:14:59 +0300

    feat(services): add customer workflow service methods

--- packages/services/src/customers/service.ts
+++ packages/services/src/customers/service.ts
@@ -1,4 +1,4 @@
-﻿import { mockCustomers } from "@insuros/mocks";
+﻿import { mockCustomerWorkflows, mockCustomers } from "@insuros/mocks";
 
 export class CustomerService {
   async getCustomers() {
@@ -7,5 +7,13 @@ export class CustomerService {
 
   async getCustomer(id: string) {
     return mockCustomers.find((customer) => customer.id === id);
+  }
+
+  async getCustomerWorkflows() {
+    return mockCustomerWorkflows;
+  }
+
+  async getCustomerWorkflow(customerId: string) {
+    return mockCustomerWorkflows.find((workflow) => workflow.customerId === customerId);
   }
 }
