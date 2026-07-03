2dea02c4f3bb45e6ff90516d9508753d36ccb9d5
Fred <bucrepinfo@gmail.com>
2026-07-03 16:35:49 +0300

    fix(services): type workflow state machine inputs

--- packages/services/src/workflow-state-machine.ts
+++ packages/services/src/workflow-state-machine.ts
@@ -1,8 +1,8 @@
 import type {
   WorkflowStateMachine,
+  WorkflowStatus,
   WorkflowTransitionRule,
-  WorkflowType,
-  WorkflowStatus
+  WorkflowType
 } from "@insuros/domain";
 
 export class WorkflowStateMachineService {
