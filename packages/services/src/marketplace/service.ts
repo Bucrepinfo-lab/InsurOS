56d169d8afda412063488219567eed0ac4c3de75
Fred <bucrepinfo@gmail.com>
2026-07-03 21:41:58 +0300

    feat(services): add marketplace workflow service methods

--- packages/services/src/marketplace/service.ts
+++ packages/services/src/marketplace/service.ts
@@ -1,11 +1,19 @@
-import { mockProducts } from "@insuros/mocks";
+import { mockMarketplaceProducts, mockMarketplaceWorkflows } from "@insuros/mocks";
 
 export class MarketplaceService {
   async getProducts() {
-    return mockProducts;
+    return mockMarketplaceProducts;
   }
 
   async getProduct(id: string) {
-    return mockProducts.find((p) => p.id === id);
+    return mockMarketplaceProducts.find((product) => product.id === id);
+  }
+
+  async getMarketplaceWorkflows() {
+    return mockMarketplaceWorkflows;
+  }
+
+  async getMarketplaceWorkflow(productId: string) {
+    return mockMarketplaceWorkflows.find((workflow) => workflow.productId === productId);
   }
 }
