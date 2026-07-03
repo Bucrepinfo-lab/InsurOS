de545e0dceec6f59c6dc8ae82bb00bfb6570f12a
Fred <bucrepinfo@gmail.com>
2026-07-03 20:32:39 +0300

    feat(services): add policy workflow service methods

--- packages/services/src/policies/service.ts
+++ packages/services/src/policies/service.ts
@@ -1,4 +1,4 @@
-﻿import { mockPolicies } from "@insuros/mocks";
+﻿import { mockPolicies, mockPolicyWorkflows } from "@insuros/mocks";
 
 export class PolicyService {
   async getPolicies() {
@@ -7,5 +7,13 @@ export class PolicyService {
 
   async getPolicy(id: string) {
     return mockPolicies.find((policy) => policy.id === id);
+  }
+
+  async getPolicyWorkflows() {
+    return mockPolicyWorkflows;
+  }
+
+  async getPolicyWorkflow(policyId: string) {
+    return mockPolicyWorkflows.find((workflow) => workflow.policyId === policyId);
   }
 }
